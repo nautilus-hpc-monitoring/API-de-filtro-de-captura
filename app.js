76134e6055683c40833875ed344089cf496bd71a
raphael-canuto <raphaelcanuto464@gmail.com>
2026-08-31 00:56:11 -0300

    Feat: arquivos iniciais

---
+++ app.js
@@ -0,0 +1,26 @@
+// var ambiente_processo = 'producao';
+var ambiente_processo = 'desenvolvimento';
+
+var caminho_env = ambiente_processo === 'producao' ? '.env' : '.env.dev';
+// Acima, temos o uso do operador ternário para definir o caminho do arquivo .env
+// A sintaxe do operador ternário é: condição ? valor_se_verdadeiro : valor_se_falso
+
+require("dotenv").config({ path: caminho_env });
+
+var express = require("express");
+var cors = require("cors");
+var path = require("path");
+var PORTA_APP = process.env.APP_PORT;
+var HOST_APP = process.env.APP_HOST;
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(cors());
+
+app.listen(PORTA_APP, function () {
+    console.log(`http://${HOST_APP}:${PORTA_APP} ${process.env.AMBIENTE_PROCESSO}`)
+});
