9de89015a849ad827bb6dff32d9ed8fdb2e23697
raphael-canuto <raphaelcanuto464@gmail.com>
2026-08-31 16:47:36 -0300

    feat: rota de listarLimites de atenção

--- src/controllers/empresaController.js
+++ src/controllers/empresaController.js
@@ -15,6 +15,22 @@ async function listarEmpresa(req, res) {
     }
 }
 
+async function listarLimites(req, res) {
+    try {
+        const idEmpresa = req.params.idEmpresa;
+        const response = await empresaModel.listarLimites(idEmpresa);
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({
+            status: 'error',
+            message: 'Ocorreu um erro interno'
+        })
+    }
+}
+
 module.exports = {
-    listarEmpresa
+    listarEmpresa,
+    listarLimites
 }
